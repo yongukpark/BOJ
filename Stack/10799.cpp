732fe09c5a49e44ca7b1276cfebb3ffa5b78c785
yongukpark <86973831+yongukpark@users.noreply.github.com>
2022-07-03 14:13:58 +0900

    Create 10799.cpp

---
+++ Stack/10799.cpp
@@ -0,0 +1,35 @@
+//원리만 이해하면 굉장히 쉽게 풀렸음
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    
+    stack<char>s1;
+    
+    bool flg = true;
+    int sum = 0;
+    
+    for(int i = 0 ; i < s.length() ; i++){
+        if(s[i] == '('){
+            s1.push('(');
+            flg = true;
+        }
+        else if(s[i] == ')'){
+            if(flg){
+                s1.pop();
+                sum+= s1.size();
+                flg = false;
+            }
+            else{
+                s1.pop(); 
+                sum++;
+            }
+        }
+    }
+    cout << sum;
+    
+}
