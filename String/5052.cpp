17cad7ad4bfcbd3456391e43b2c67da00a495303
yongukpark <86973831+yongukpark@users.noreply.github.com>
2022-07-13 22:07:26 +0900

    Create 5052.cpp

---
+++ String/5052.cpp
@@ -0,0 +1,58 @@
+//정렬 카테고리를 공부하고 있지만 정렬보다는 다른 것들 위주로 더 쓰이는 것 같음
+//이 문제 같은 경우에도 단순 문자열 문제로 보임
+//트라이라는 문자열을 다루는 자료구조에 대해 알았음 / 추후 다시 공부예정
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int testCase; cin >> testCase;
+    for(int i = 0 ; i < testCase ; i++)
+    {
+        bool totalFlg = true;
+        string arr[10003];
+        int num; cin >> num;
+        for(int j = 0 ; j < num ; j++)
+        {
+            cin >> arr[j];
+        }
+        sort(arr,arr + num);
+        for(int j = 0 ; j < num - 1 ; j++)
+        {
+            
+
+            if(arr[j].length() >= arr[j+1].length())
+            {
+                continue;
+            }
+            else{
+                bool flg = false;
+                for(int k = 0 ; k < arr[j].length() ; k++)
+                {
+                    if(arr[j][k] != arr[j+1][k])
+                    {
+                        flg = true;
+                        break;
+                    }
+                }
+                if(!flg)
+                {
+                    totalFlg = false;
+                }
+            }
+            if(!totalFlg)
+            {
+                break;
+            }
+        }
+        if(totalFlg)
+        {
+           cout << "YES" << '\n'; 
+        }
+        else{
+            cout << "NO" << '\n';
+        }
+    }
+}
