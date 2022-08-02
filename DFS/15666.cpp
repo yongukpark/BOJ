24ed1a611d2eab2ffe522af06369db13f493c5f9
yongukpark <86973831+yongukpark@users.noreply.github.com>
2022-08-02 22:58:18 +0900

    Create 15666.cpp

---
+++ DFS/15666.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[10];
+int n,m;
+int res[10];
+
+void dfs(int count, int a)
+{
+    if(count == m)
+    {
+        for(int i = 0 ; i < m ; i++)
+        {
+            cout << res[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    int postNum = 0;
+    for(int i = a ; i < n ; i++)
+    {
+        if(arr[i] == postNum)
+        {
+            continue;
+        }
+        res[count] = arr[i];
+        postNum = arr[i];
+        dfs(count + 1 , i);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> arr[i];   
+    }
+    
+    sort(arr,arr+n);
+    dfs(0,0);
+}
